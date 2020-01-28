266bb7d9961220dcfc60fbf378779ae384fd4a57
Milo Rastgoo <hello@informagician.com>
2020-01-28 18:59:15 -0500

    delete function added

--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -16,6 +16,12 @@ const FriendList = () => {
         })
         .catch(err => console.log(err))
     }, [update])
+
+    const handelDelete = id => {
+        axiosWithAuth().delete('api/friends/' + id)
+        .then(res => setUpdate(res))
+        .catch(err => console.log(err))
+    }
     
     return(
         <div className="container">
@@ -24,7 +30,7 @@ const FriendList = () => {
                 <div className="friends">
                     {friends.map(friend => (
                         <div key={friend.id}>
-                            <p className="name">{friend.name}</p>
+                            <p className="name">{friend.name}<span className="delete" onClick={() => handelDelete(friend.id)}>[delete]</span></p>
                             <p>Age: {friend.age}</p>
                             <p>Email: {friend.email}</p>
                         </div>
